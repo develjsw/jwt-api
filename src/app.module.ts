d47afdbca693749134e50a2ed45a7f5b8ffe4849
jsw <develjsw@gmail.com>
2024-06-12 21:21:58 +0900

    config 파일명 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,22 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import configLocal from './config/local/config.local';
-import configDevelopment from './config/development/config.development';
-import configProduction from './config/production/config.production';
+import localConfig from './config/local/local.config';
+import developmentConfig from './config/development/development.config';
+import productionConfig from './config/production/production.config';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from './common/logger/logger.middleware';
 
 let config;
 switch (process.env.NODE_ENV) {
     case 'development':
-        config = configDevelopment;
+        config = productionConfig;
         break;
     case 'production':
-        config = configProduction;
+        config = developmentConfig;
         break;
     default:
-        config = configLocal;
+        config = localConfig;
         break;
 }
 
